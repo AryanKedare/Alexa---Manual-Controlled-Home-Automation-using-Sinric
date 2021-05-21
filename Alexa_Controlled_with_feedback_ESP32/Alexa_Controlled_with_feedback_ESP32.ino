99fed0c01cfca12a643c299dde3ff0f4da9a815e
Aryan Kedare <arykedare@gmail.com>
2021-05-21 12:38:28 +0530

    Update Code TO V2 (sinric.pro)

--- Alexa_Controlled_with_feedback_ESP32/Alexa_Controlled_with_feedback_ESP32.ino
+++ Alexa_Controlled_with_feedback_ESP32/Alexa_Controlled_with_feedback_ESP32.ino
@@ -1,230 +1,172 @@
 /*
-  This is the code for the project called
-
-  Alexa & Manual Controlled Home Automation using Sinric
-
-  This code is written by Sachin Soni on 04.07.2020
-
-  The tutorial Video for the project is uploaded on 
-  our YouTube channel called "techiesms"
-
-  Channel Link - https://www.youtube.com/techiesms
-
-
-          techiesms
-  explore | learn | share 
+ This is the code for the project called
+ Alexa & Manual Controlled Home Automation using Sinric.pro
+ This Code is Modified by Aryan Kedare using the examples of esp32 sdk provided on sinric.pro github account and NodeMCU based code of iotcircuithub.com 
+ to work with the PCB of Home Automation Provided of Sachin Soni (techiesms) on 21.05.2021
 */
+
+
 
 #include <Arduino.h>
 #include <WiFi.h>
-#include <WiFiMulti.h>
-#include <WebSocketsClient.h> //  https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
-#include <ArduinoJson.h> // https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
-#include <StreamString.h>
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
 
-#include <AceButton.h> // https://github.com/bxparks/AceButton
-using namespace ace_button;
-WiFiMulti WiFiMulti;
-WebSocketsClient webSocket;
-WiFiClient client;
+#include <map>
 
-#define MyApiKey "Your_API_Key" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
-#define MySSID "SSID" // TODO: Change to your Wifi network SSID
-#define MyWifiPassword "PASS" // TODO: Change to your Wifi network password
+#define WIFI_SSID         "SSID"    
+#define WIFI_PASS         "PASS"
+#define APP_KEY           "APP_KEY"      // get it from sinric.pro
+#define APP_SECRET        "APP_SECRET"   // get it from sinric.pro
 
-#define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
+//Device ID's From sinric.pro
+#define device_ID_1   "xxxxxxxxxxxxxxxxxxxxxxxx"
+#define device_ID_2   "xxxxxxxxxxxxxxxxxxxxxxxx"
+#define device_ID_3   "xxxxxxxxxxxxxxxxxxxxxxxx"
+#define device_ID_4   "xxxxxxxxxxxxxxxxxxxxxxxx"
 
-uint64_t heartbeatTimestamp = 0;
-bool isConnected = false;
+// GPIO Pin configuration 
+#define RelayPin1 15  //D15
+#define RelayPin2 2  //D2
+#define RelayPin3 4 //D5
+#define RelayPin4 22 //D22
 
-// Switch
-const int BUTTON1_PIN = 32;
-const int BUTTON2_PIN = 35;
-const int BUTTON3_PIN = 34;
-const int BUTTON4_PIN = 39;
+#define SwitchPin1 32  //D32
+#define SwitchPin2 35  //D35
+#define SwitchPin3 34  //D34
+#define SwitchPin4 39  //VIN
 
-
-//Relays
-const int RELAY1_PIN = 15;
-const int RELAY2_PIN = 2;
-const int RELAY3_PIN = 4;
-const int RELAY4_PIN = 22;
-
-//Status LEDs
+//LEDs Status
 const int LED1 = 26;
 const int LED2 = 25;
 const int LED3 = 27;
 
 
-String device_ID_1 = "DeviceID1";
-String device_ID_2 = "DeviceID2";
-String device_ID_3 = "DeviceID3";
-String device_ID_4 = "DeviceID4";
+// comment the following line if you use a toggle switches instead of tactile buttons
+//#define TACTILE_BUTTON 1
+
+#define BAUD_RATE   9600
+
+#define DEBOUNCE_TIME 250
+
+typedef struct {      // struct for the std::map below
+  int relayPIN;
+  int flipSwitchPIN;
+} deviceConfig_t;
 
 
+// this is the main configuration
+// please put in your deviceId, the PIN for Relay and PIN for flipSwitch
+// this can be up to N devices...depending on how much pin's available on your device ;)
+// right now we have 4 devicesIds going to 4 relays and 4 flip switches to switch the relay manually
+std::map<String, deviceConfig_t> devices = {
+    //{deviceId, {relayPIN,  flipSwitchPIN}}
+    {device_ID_1, {  RelayPin1, SwitchPin1 }},
+    {device_ID_2, {  RelayPin2, SwitchPin2 }},
+    {device_ID_3, {  RelayPin3, SwitchPin3 }},
+    {device_ID_4, {  RelayPin4, SwitchPin4 }}     
+};
 
-ButtonConfig config1;
-AceButton button1(&config1);
-ButtonConfig config2;
-AceButton button2(&config2);
-ButtonConfig config3;
-AceButton button3(&config3);
-ButtonConfig config4;
-AceButton button4(&config4);
+typedef struct {      // struct for the std::map below
+  String deviceId;
+  bool lastFlipSwitchState;
+  unsigned long lastFlipSwitchChange;
+} flipSwitchConfig_t;
 
+std::map<int, flipSwitchConfig_t> flipSwitches;    // this map is used to map flipSwitch PINs to deviceId and handling debounce and last flipSwitch state checks
+                                                  // it will be setup in "setupFlipSwitches" function, using informations from devices map
 
-
-
-void handleEvent1(AceButton*, uint8_t, uint8_t);
-void handleEvent2(AceButton*, uint8_t, uint8_t);
-void handleEvent3(AceButton*, uint8_t, uint8_t);
-void handleEvent4(AceButton*, uint8_t, uint8_t);
-
-void setPowerStateOnServer(String deviceId, String value);
-
-// deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
-
-void turnOn(String deviceId) {
-  if (deviceId == device_ID_1) // Device ID of first device
-  {
-    Serial.print("Turn on device id: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY1_PIN, LOW);
+void setupRelays() { 
+  for (auto &device : devices) {           // for each device (relay, flipSwitch combination)
+    int relayPIN = device.second.relayPIN; // get the relay pin
+    pinMode(relayPIN, OUTPUT);             // set relay pin to OUTPUT
+    digitalWrite(relayPIN, HIGH);
   }
-  if (deviceId == device_ID_2) // Device ID of first device
-  {
-    Serial.print("Turn on device id: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY2_PIN, LOW);
-  }
-  if (deviceId == device_ID_3) // Device ID of first device
-  {
-    Serial.print("Turn on device id: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY3_PIN, LOW);
-  }
-  if (deviceId == device_ID_4) // Device ID of first device
-  {
-    Serial.print("Turn on device id: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY4_PIN, LOW);
-  }
-
 }
 
-void turnOff(String deviceId) {
-  if (deviceId == device_ID_1) // Device ID of first device
-  {
-    Serial.print("Turn off Device ID: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY1_PIN, HIGH);
-  }
-  if (deviceId == device_ID_2) // Device ID of first device
-  {
-    Serial.print("Turn off Device ID: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY2_PIN, HIGH);
-  }
-  if (deviceId == device_ID_3) // Device ID of first device
-  {
-    Serial.print("Turn off Device ID: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY3_PIN, HIGH);
-  }
-  if (deviceId == device_ID_4) // Device ID of first device
-  {
-    Serial.print("Turn off Device ID: ");
-    Serial.println(deviceId);
-    digitalWrite(RELAY4_PIN, HIGH);
-  }
+void setupFlipSwitches() {
+  for (auto &device : devices)  {                     // for each device (relay / flipSwitch combination)
+    flipSwitchConfig_t flipSwitchConfig;              // create a new flipSwitch configuration
 
+    flipSwitchConfig.deviceId = device.first;         // set the deviceId
+    flipSwitchConfig.lastFlipSwitchChange = 0;        // set debounce time
+    flipSwitchConfig.lastFlipSwitchState = true;     // set lastFlipSwitchState to false (LOW)--
+
+    int flipSwitchPIN = device.second.flipSwitchPIN;  // get the flipSwitchPIN
+
+    flipSwitches[flipSwitchPIN] = flipSwitchConfig;   // save the flipSwitch config to flipSwitches map
+    pinMode(flipSwitchPIN, INPUT_PULLUP);                   // set the flipSwitch pin to INPUT
+  }
 }
 
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      isConnected = false;
-      WiFiMulti.addAP(MySSID, MyWifiPassword);
-      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
-      break;
-    case WStype_CONNECTED: {
-        isConnected = true;
-        Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
-        Serial.printf("Waiting for commands from sinric.com ...\n");
-      }
-      break;
-    case WStype_TEXT: {
-        Serial.printf("[WSc] get text: %s\n", payload);
-        // Example payloads
+bool onPowerState(String deviceId, bool &state)
+{
+  Serial.printf("%s: %s\r\n", deviceId.c_str(), state ? "on" : "off");
+  int relayPIN = devices[deviceId].relayPIN; // get the relay pin for corresponding device
+  digitalWrite(relayPIN, !state);             // set the new relay state
+  return true;
+}
 
-        // For Switch or Light device types
-        // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
+void handleFlipSwitches() {
+  unsigned long actualMillis = millis();                                          // get actual millis
+  for (auto &flipSwitch : flipSwitches) {                                         // for each flipSwitch in flipSwitches map
+    unsigned long lastFlipSwitchChange = flipSwitch.second.lastFlipSwitchChange;  // get the timestamp when flipSwitch was pressed last time (used to debounce / limit events)
 
-        // For Light device type
-        // Look at the light example in github
+    if (actualMillis - lastFlipSwitchChange > DEBOUNCE_TIME) {                    // if time is > debounce time...
 
-#if ARDUINOJSON_VERSION_MAJOR == 5
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject((char*)payload);
-#endif
-#if ARDUINOJSON_VERSION_MAJOR == 6
-        DynamicJsonDocument json(1024);
-        deserializeJson(json, (char*) payload);
-#endif
-        String deviceId = json ["deviceId"];
-        String action = json ["action"];
+      int flipSwitchPIN = flipSwitch.first;                                       // get the flipSwitch pin from configuration
+      bool lastFlipSwitchState = flipSwitch.second.lastFlipSwitchState;           // get the lastFlipSwitchState
+      bool flipSwitchState = digitalRead(flipSwitchPIN);                          // read the current flipSwitch state
+      if (flipSwitchState != lastFlipSwitchState) {                               // if the flipSwitchState has changed...
+#ifdef TACTILE_BUTTON
+        if (flipSwitchState) {                                                    // if the tactile button is pressed 
+#endif      
+          flipSwitch.second.lastFlipSwitchChange = actualMillis;                  // update lastFlipSwitchChange time
+          String deviceId = flipSwitch.second.deviceId;                           // get the deviceId from config
+          int relayPIN = devices[deviceId].relayPIN;                              // get the relayPIN from config
+          bool newRelayState = !digitalRead(relayPIN);                            // set the new relay State
+          digitalWrite(relayPIN, newRelayState);                                  // set the trelay to the new state
 
-        if (action == "setPowerState") { // Switch or Light
-          String value = json ["value"];
-          if (value == "ON") {
-            turnOn(deviceId);
-          } else {
-            turnOff(deviceId);
-          }
+          SinricProSwitch &mySwitch = SinricPro[deviceId];                        // get Switch device from SinricPro
+          mySwitch.sendPowerStateEvent(!newRelayState);                            // send the event
+#ifdef TACTILE_BUTTON
         }
-        else if (action == "test") {
-          Serial.println("[WSc] received test command from sinric.com");
-        }
+#endif      
+        flipSwitch.second.lastFlipSwitchState = flipSwitchState;                  // update lastFlipSwitchState
       }
-      break;
-    case WStype_BIN:
-      Serial.printf("[WSc] get binary length: %u\n", length);
-      break;
+    }
   }
 }
 
-void setup() {
-  Serial.begin(9600);
+void setupWiFi()
+{
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  WiFiMulti.addAP(MySSID, MyWifiPassword);
-  Serial.println();
-  Serial.print("Connecting to Wifi: ");
-  Serial.println(MySSID);
-
-  // Waiting for Wifi connect
-  if (WiFiMulti.run() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("Connecting...");
+  while (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.printf(".");
+    delay(250);
   }
-  if (WiFiMulti.run() == WL_CONNECTED) {
-    Serial.println("");
-    Serial.print("WiFi connected. ");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+void setupSinricPro()
+{
+  for (auto &device : devices)
+  {
+    const char *deviceId = device.first.c_str();
+    SinricProSwitch &mySwitch = SinricPro[deviceId];
+    mySwitch.onPowerState(onPowerState);
   }
 
-  // Setup button
-  // Button uses the built-in pull up register.
-  pinMode(BUTTON1_PIN, INPUT_PULLUP); // INPUT_PULLUP so no need a 10K resistor
-  pinMode(BUTTON2_PIN, INPUT_PULLUP); // INPUT_PULLUP so no need a 10K resistor
-  pinMode(BUTTON3_PIN, INPUT_PULLUP); // INPUT_PULLUP so no need a 10K resistor
-  pinMode(BUTTON4_PIN, INPUT_PULLUP); // INPUT_PULLUP so no need a 10K resistor
+  SinricPro.begin(APP_KEY, APP_SECRET);
+  SinricPro.restoreDeviceStates(true);
+}
 
-  pinMode(RELAY1_PIN, OUTPUT);
-  pinMode(RELAY2_PIN, OUTPUT);
-  pinMode(RELAY3_PIN, OUTPUT);
-  pinMode(RELAY4_PIN, OUTPUT);
-
+void setup()
+{
+  Serial.begin(BAUD_RATE);
    pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
@@ -252,34 +194,15 @@ void setup() {
   digitalWrite(LED2, LOW);
   digitalWrite(LED3, LOW);
 
-
-  config1.setEventHandler(button1Handler);
-  config2.setEventHandler(button2Handler);
-  config3.setEventHandler(button3Handler);
-  config4.setEventHandler(button4Handler);
-
-
-  button1.init(BUTTON1_PIN);
-  button2.init(BUTTON2_PIN);
-  button3.init(BUTTON3_PIN);
-  button4.init(BUTTON4_PIN);
-
-
-
-  // server address, port and URL
-  webSocket.begin("iot.sinric.com", 80, "/");
-
-  // event handler
-  webSocket.onEvent(webSocketEvent);
-  webSocket.setAuthorization("apikey", MyApiKey);
-
-  // try again every 5000ms if connection has failed
-  webSocket.setReconnectInterval(5000);   // If you see 'class WebSocketsClient' has no member named 'setReconnectInterval' error update arduinoWebSockets
+  setupRelays();
+  setupFlipSwitches();
+  setupWiFi();
+  setupSinricPro();
 }
 
 void loop() {
-
-  if (WiFiMulti.run() != WL_CONNECTED)
+  
+  if (WiFi.status() != WL_CONNECTED)
   {
     Serial.println("Not Connected");
     digitalWrite(LED1, HIGH);
@@ -292,109 +215,12 @@ void loop() {
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
-    webSocket.loop();
   }
 
 
-  button1.check();
-  button2.check();
-  button3.check();
-  button4.check();
 
-
-  if (isConnected) {
-    uint64_t now = millis();
-
-    // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
-    if ((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
-      heartbeatTimestamp = now;
-      webSocket.sendTXT("H");
-    }
-  }
+{
+  SinricPro.handle();
+  handleFlipSwitches();
 }
-
-void setPowerStateOnServer(String deviceId, String value) {
-#if ARDUINOJSON_VERSION_MAJOR == 5
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-#endif
-#if ARDUINOJSON_VERSION_MAJOR == 6
-  DynamicJsonDocument root(1024);
-#endif
-
-  root["deviceId"] = deviceId;
-  root["action"] = "setPowerState";
-  root["value"] = value;
-  StreamString databuf;
-#if ARDUINOJSON_VERSION_MAJOR == 5
-  root.printTo(databuf);
-#endif
-#if ARDUINOJSON_VERSION_MAJOR == 6
-  serializeJson(root, databuf);
-#endif
-  webSocket.sendTXT(databuf);
-}
-
-void button1Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
-  Serial.println("EVENT1");
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      Serial.println("kEventPressed");
-      setPowerStateOnServer(device_ID_1, "ON");
-      digitalWrite(RELAY1_PIN, LOW);
-      break;
-    case AceButton::kEventReleased:
-      Serial.println("kEventReleased");
-      setPowerStateOnServer(device_ID_1, "OFF");
-      digitalWrite(RELAY1_PIN, HIGH);
-      break;
-  }
-}
-
-void button2Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
-  Serial.println("EVENT2");
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      Serial.println("kEventPressed");
-      setPowerStateOnServer(device_ID_2, "ON");
-      digitalWrite(RELAY2_PIN, LOW);
-      break;
-    case AceButton::kEventReleased:
-      Serial.println("kEventReleased");
-      setPowerStateOnServer(device_ID_2, "OFF");
-      digitalWrite(RELAY2_PIN, HIGH);
-      break;
-  }
-}
-
-void button3Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
-  Serial.println("EVENT3");
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      Serial.println("kEventPressed");
-      setPowerStateOnServer(device_ID_3, "ON");
-      digitalWrite(RELAY3_PIN, LOW);
-      break;
-    case AceButton::kEventReleased:
-      Serial.println("kEventReleased");
-      setPowerStateOnServer(device_ID_3, "OFF");
-      digitalWrite(RELAY3_PIN, HIGH);
-      break;
-  }
-}
-
-void button4Handler(AceButton* button, uint8_t eventType, uint8_t buttonState) {
-  Serial.println("EVENT4");
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      Serial.println("kEventPressed");
-      setPowerStateOnServer(device_ID_4, "ON");
-      digitalWrite(RELAY4_PIN, LOW);
-      break;
-    case AceButton::kEventReleased:
-      Serial.println("kEventReleased");
-      setPowerStateOnServer(device_ID_4, "OFF");
-      digitalWrite(RELAY4_PIN, HIGH);
-      break;
-  }
 }
